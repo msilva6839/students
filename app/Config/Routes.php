19adad77da7308b29593df652783990e1f2ace4b
msilva <msilva6839@utm.edu.ec>
2024-06-08 05:57:03 -0500

    Actualizacion de las rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,21 +10,18 @@ $routes->get('/', 'Home::index');
 // Rutas para personas
 $routes->get('personas', 'Home::personas_index');
 $routes->get('personas/(:num)', 'Home::personas_show/$1');
-$routes->post('personas', 'Home::personas_create');
 $routes->put('personas/(:num)', 'Home::personas_update/$1');
-$routes->delete('personas/(:num)', 'Home::personas_delete/$1');
+
 
 // Rutas para empleos
 $routes->get('empleos', 'Home::empleos_index');
 $routes->get('empleos/(:num)', 'Home::empleos_show/$1');
-$routes->post('empleos', 'Home::empleos_create');
 $routes->put('empleos/(:num)', 'Home::empleos_update/$1');
-$routes->delete('empleos/(:num)', 'Home::empleos_delete/$1');
+
 
 
 // Ruta para crear un nuevo registro de persona y empleo
 $routes->post('crear', 'Home::registro_create');
-
 $routes->delete('eliminar/(:num)', 'Home::registro_delete/$1');
 
 
